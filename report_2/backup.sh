2e4ecd76956726686e5b2980abb9044b873b99b1
nixgnaf <145898902+nixgnaf@users.noreply.github.com>
2024-09-10 10:34:39 -0700

    2

---
+++ report_2/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BACKUPFILE=backup-$(date +%m-%d-%Y)
+
+archive=${1:-$BACKUPFILE}
+
+tar cvf - `find . -mtime -1 -type f -print` > $archive.tar
+gzip $archive.tar
+echo "Directory $PWD backed up in archive file \"$archive.tar.gz\"."
+
+exit 0
